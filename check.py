49427bf5dea7def4731b9db00b5aaddb58d4f187
Dana Makov <dana.makov@gmail.com>
2020-07-28 18:24:51 +0300

    part 3 - check the DB

--- check.py
+++ check.py
@@ -2,9 +2,10 @@ import sqlite3
 import contextlib
 from configuration import *
 
+table = "products"
 with contextlib.closing(sqlite3.connect(DB_FILENAME)) as con:  # auto-closes
     with con:
         cur = con.cursor()
-        cur.execute("select * from products")
+        cur.execute(f"select * from {table}")
         for i in cur.fetchall():
             print(i)
